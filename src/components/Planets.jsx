e68d5682f71731d184a6522d12c33bb0035115f4
Philomena Oluchukwu <philomenaoluchukwu91@gmail.com>
2026-03-27 22:01:29 +0100

    style: final planets updates

--- src/components/Planets.jsx
+++ src/components/Planets.jsx
@@ -3,7 +3,6 @@ import PlanetCard from './PlanetCard';
 import LoadingSpinner from './LoadingSpinner';
 import ErrorMessage from './ErrorMessage';
 import '../styles/Planets.css';
-
 function Planets() {
   const [planets, setPlanets] = useState([]);
   const [loading, setLoading] = useState(true);
