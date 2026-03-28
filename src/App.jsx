d5d8a802bc3e2b4e5c4a80beb6c6f7263ca79e33
David fadoju <117608846+Bobo2005@users.noreply.github.com>
2026-03-28 23:26:54 +0100

    planet-table

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import PlanetTable from "./components/PlanetTable";
 import './App.css'
 
 function App() {
