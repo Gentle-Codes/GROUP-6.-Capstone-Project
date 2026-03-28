62b4c2fe8d48ae196aedc69c192161053e63c34f
adeyemiabayomi2025@gmail.com <adeyemiabayomi2025@hmail.com>
2026-03-29 00:23:08 +0100

    fix: import all component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import VideoSection from './components/VideoSection';
+import Planets from './components/Planets';
+import PlanetTable from './components/PlanetTable';
+import ContactForm from './components/ContactForm';
+import Footer from './components/footer';
 import './App.css';
 
 function App() {
   return (
     <>
+      <Navbar />
+      <Hero />
+      <VideoSection />
+      <Planets />
+      <PlanetTable />
+      <ContactForm />
       <Footer />
     </>
   );
 }
 
+export default App;
